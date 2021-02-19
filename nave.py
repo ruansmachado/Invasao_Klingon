8eb417b2e6e1ccd5b830bcdbb27dfa7a29e7946c
Ruan Machado <ruansmachado@yahoo.com>
2021-02-19 20:14:50 -0300

    Add files via upload

--- nave.py
+++ nave.py
@@ -6,7 +6,7 @@ class Nave:
     def __init__(self, ai_game):
         """Inicia a nave e as configurações da posição inicial"""
         self.screen = ai_game.screen
-        self.configuracoes = ai_game.configuracoes
+        self.settings = ai_game.configuracoes
         self.screen_rect = ai_game.screen.get_rect()
         # Carrega a imagem da nave e mapea a posição
         self.image = pygame.transform.rotate(pygame.transform.scale(pygame.image.load('images/aventine_ship.bmp'),
@@ -25,11 +25,16 @@ class Nave:
         """Atualiza a posição da nave baseado no movimento indicado"""
         # Atualiza o valor x da nave, não o rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.configuracoes.nave_velocidade
+            self.x += self.settings.nave_velocidade
         if self.moving_left and self.rect.left > 0:
-            self.x -= self.configuracoes.nave_velocidade
+            self.x -= self.settings.nave_velocidade
         # Atualiza o objeto rect da self.x
         self.rect.x = self.x
+
+    def center_nave(self):
+        """Centraliza a nave"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
     def blitme(self):
         """Posiciona a nave na sua posição padrão"""
