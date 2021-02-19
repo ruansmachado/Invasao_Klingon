2888bb542bd66f56a96a84903d26084f7c80601c
Ruan Machado <ruansmachado@yahoo.com>
2021-02-18 21:53:45 -0300

    Add files via upload

---
+++ configuracoes.py
@@ -0,0 +1,11 @@
+class Configuracoes:
+    """Class que armazena todas as configurações do Invasão Klingon"""
+
+    def __init__(self):
+        """Inicia as configurações do jogo"""
+        # Configurações de tela
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        # Configurações da nave
+        self.nave_velocidade = 1.5
